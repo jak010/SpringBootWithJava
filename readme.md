3b74feac44f7ff0ffbc846fd0fd1d279212f0889
jak010 <bluetoon@naver.com>
2021-11-22 01:58:20 -0500

    init

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 
-###  스프링부트로 배우는 자바 웹 개발
+### SpringBoot Study Source
+- `basic`: 스프링부트로 배우는 자바 웹 개발
--- src/main/java/com/example/demo/model/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model;
-
-public enum UserRole {
-    USER, // 0
-    ADMIN  // 1
-
-}
